ed445063023706c3d432fef5be12b524456165ca
Andrew McKnight <thedonk@gmail.com>
2024-04-29 11:31:12 -0700

    Forgot to commit new Exception class

---
+++ Components/SMW/SNES/EmuException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SNES {
+    [Serializable]
+    internal class EmuException : Exception {
+        public EmuException() {
+        }
+
+        public EmuException(string message) : base(message) {
+        }
+
+        public EmuException(string message, Exception innerException) : base(message, innerException) {
+        }
+
+        protected EmuException(SerializationInfo info, StreamingContext context) : base(info, context) {
+        }
+    }
+}
